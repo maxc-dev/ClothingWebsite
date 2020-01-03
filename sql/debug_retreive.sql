eb3cb40a0fd25f621178a2bbf771d1dfc64615d9
Max Carter <32675787+maxc-dev@users.noreply.github.com>
2020-01-03 00:52:07 +0000

    Add files via upload

--- sql/debug_retreive.sql
+++ sql/debug_retreive.sql
@@ -1,7 +1,1 @@
 USE maxc_dev_localhost;
-
-SELECT Item.ItemID, Item.ItemName, Item.OriginalPrice, Item.Category, Item.Gender, Brand.BrandName, Colour.ColourID, Colour.ColourName
- FROM Item
- INNER JOIN LinkItemColour ON Item.ItemID=LinkItemColour.ItemID
- INNER JOIN Colour ON Colour.ColourID=LinkItemColour.ColourID
- INNER JOIN Brand ON Brand.BrandID=Item.BrandID;
--- sql/populate.sql
+++ sql/populate.sql
@@ -8,18 +8,25 @@ populating the brands table.
 */
 INSERT INTO Brand (BrandName) VALUES ("Emporio Armani"), ("Calvin Klein"), ("Champion"), ("Fred Perry"), ("Hollister"), ("Nike"), ("Ralph Lauren"), ("Superdry"), ("Ted Baker"), ("Tommy Hilfiger"), ("The North Face");
 
+INSERT INTO Size (SizeName) VALUES ("Small"), ("Medium"), ("Large"), ("8"), ("9"), ("10"), ('28"'), ('30"'), ('32');
+
+INSERT INTO Category (CategoryName) VALUES
+("Jackets"), ("Hoodie"), ("T-Shirt"), ("Polo Shirt"), ("Jeans"), ("Joggers"), ("Trainers"), ("Boots");
+
 INSERT INTO User (Username, Email, Password) VALUES ("Max", "max@gmail.com", "admin"), ("Elle", "elle@gmail.com", "fox"), ("Louis", "louis@gmail.com", "chief"), ("Ryan", "ryan@gmail.com", "ginger"), ("Vivi", "vivi@gmail.com", "nick");
 
 INSERT INTO Colour (ColourName) VALUES ("Blue"), ("Red"), ("White"), ("Black"), ("Navy"), ("Grey"), ("Green");
 
-INSERT INTO Item (ItemName, OriginalPrice, Category, Gender, BrandID) VALUES
-("EA7 Large Logo Hoodie", 129.99, "Tops", "Male", 1),
-("Large Eagle Logo Hoodie", 149.99, "Tops", "Male", 1);
+INSERT INTO Item (OriginalPrice, CategoryID, Gender, BrandID) VALUES
+(129.99, 2, "Male", 1),
+(159.99, 2, "Male", 1);
  
 INSERT INTO LinkItemColour (ItemID, ColourID) VALUES (1, 5), (2, 4);
 
-INSERT INTO Size (ItemID, ColourID, SizeName, SizeQuantity) VALUES
--- (1, 5, "Small", 10), (1, 5, "Medium", 12), (1, 5, "Large", 16),
- (2, 4, "Small", 5), (2, 4, "Medium", 3), (2, 4, "Large", 11); 
+INSERT INTO LinkItemSize (ItemID, ColourID, SizeID, SizeQuantity) VALUES
+ (1, 5, 1, 10), (1, 5, 2, 12), (1, 5, 3, 16),
+ (2, 4, 1, 5), (2, 4, 2, 3), (2, 4, 3, 11); 
  
-INSERT INTO Discount (ItemID, ColourID, DiscountPercent) VALUES (1, 5, 20);
+ INSERT INTO ItemTop (ItemID, SleeveLength) VALUES (1, "Long"), (2, "Long");
+ 
+INSERT INTO Discount (ItemID, ColourID, DiscountPercent) VALUES (1, 5, 10);
